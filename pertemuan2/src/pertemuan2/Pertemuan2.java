72a1f765aa4fbbb48ca3821991cf886cf4b59e90
gootrisandi <gootrisandi@gmail.com>
2025-03-11 21:56:13 +0800

    Pertemuan 2

---
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,26 @@
+
+package pertemuan2;
+
+/**
+ *
+ * @author Trisandi Goser
+ * tgl : 11 maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * 
+     * @param args the command line arguments
+     */
+      public static void main(String[] args) {
+           String nama = "Trisandi Goser";
+           String nim = "2301010505";
+      System.out.println("Pertemuan 2 Java");
+      System.out.println("'''''''''''''''''''");
+      System.out.println("Nama : "+nama);
+      System.out.println("Nim  : "+nim);
+     System.out.printf("Nama saya Trisandi belajar bahasa java",nama,nim);
+      
+    }
+    
+}
